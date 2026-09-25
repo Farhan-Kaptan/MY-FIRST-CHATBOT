f9762f5619d61446b613cfdb198c587523a456dd
Farhan <kaptanfarhan@gmail.com>
2026-09-25 17:49:07 +0530

    Main Program

--- main.py
+++ main.py
@@ -110,4 +110,4 @@ if choice:
    full_response = ""
    for chunk in response_stream:
        full_response += chunk.text
-       response_placeholder.markdown(full_response)
+       response_placeholder.markdown(full_response) 
